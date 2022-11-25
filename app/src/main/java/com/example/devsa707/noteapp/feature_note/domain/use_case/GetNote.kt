5cd470976a45dea1b70b0793f6453aded51cfc7d
devsa707 <101226180+devsa707@users.noreply.github.com>
2022-11-25 06:57:54 -0300

    final project

---
+++ app/src/main/java/com/example/devsa707/noteapp/feature_note/domain/use_case/GetNote.kt
@@ -0,0 +1,13 @@
+package com.example.devsa707.noteapp.feature_note.domain.use_case
+
+import com.example.devsa707.noteapp.feature_note.domain.model.Note
+import com.example.devsa707.noteapp.feature_note.domain.repository.NoteRepository
+
+class GetNote(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(id: Int): Note? {
+        return repository.getNoteById(id)
+    }
+}
---
+++ app/src/main/java/com/example/devsa707/noteapp/feature_note/presentation/add_edit_note/NoteTextFieldState.kt
@@ -0,0 +1,7 @@
+package com.example.devsa707.noteapp.feature_note.presentation.add_edit_note
+
+data class NoteTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val isHintVisible: Boolean = true
+)
